30910d24b76c088492e8cd36d5432be227cd2d00
3ichael 7ambert <mlambertbusiness@gmail.com>
2023-08-06 16:03:22 -0600

    Create swiftIntro.swift

---
+++ swiftIntro.swift
@@ -0,0 +1,16 @@
+//Learing a new language is always fun
+//While learning the basics of Swift, it looks similar to  JavaScript and Python
+
+
+let username = "info"
+let domain = "3ichael7ambert"
+let score = 100
+
+print("Your username is \(username) from the domain \(domain)")
+print("If you look at the \"Source Code\" You will see how we can add \"quotations")
+
+//DONOT DO THIS
+print("Your username is " + username + " from the domain " + domain)
+// the code will fail
+
+//Swift language is for iOS
